304e5ea99cc61a71182a929918c14cd67f9b1cca
Tatiana Nemesh <nemesh.tatyana@gmail.com>
2021-12-22 00:45:31 +0100

    Add files via upload

--- 1-hello/script.js
+++ 1-hello/script.js
@@ -1,6 +1,3 @@
 const userName = prompt('What is your name?');
-if (userName) {
-    document.querySelector('#welcome').innerHTML = `Hello, ${userName}! How are you?`;
-} else {
-    document.querySelector('#welcome').innerHTML = 'Hello, guest! How are you?';
-}guest
+
+alert (userName);
